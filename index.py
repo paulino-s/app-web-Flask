298341f67a9d81df79dca42fddceab02af9ed51b
paulino <paulino-s@MacBook-Pro-de-paulino.local>
2021-07-08 20:32:12 -0600

    routes

--- index.py
+++ index.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def home():
     return 'Hello World'
 
+@app.route('/add_contact')
+def add_contact():
+    return'add contact'
+
+@app.route('/edit')
+def edit_contact():
+    return'edit contact'
+
+@app.rout('/delete')
+def delete_contact():
+    return'delecte contact'
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=3000, debug=True)
